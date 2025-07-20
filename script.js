f323c130e253a1376079043801a76421bd6d41e1
BalaMuruganGM <152595531+BalaMuruganGM@users.noreply.github.com>
2025-07-20 23:24:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 fetch('http://16.171.226.168:5300/get')
-  .then(response => response.json())
-  .then(result => {
-    let dataArray = result.data; // Correct key here
+.then(response => response.json())
+.then(result => {
+    const data = result.data; // ✅ Use 'data' not 'result'
     const table = document.querySelector('.one');
-
-    dataArray.forEach(element => {
-      const tr = document.createElement('tr');
-      tr.innerHTML = `
-        <td>${element.id}</td>
-        <td>${element.name}</td>
-        <td>${element.age}</td>
-      `;
-      table.appendChild(tr);
+    data.forEach(element => {
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+            <td>${element.id}</td>
+            <td>${element.name}</td>
+            <td>${element.age}</td>
+        `;
+        table.appendChild(tr);
     });
-  })
-  .catch(error => {
-    console.log(error);
-  });
+})
+.catch(error => {
+    console.log("Fetch error:", error);
+});
